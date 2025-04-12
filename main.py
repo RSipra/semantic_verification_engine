5a07f12b29aa3f5f274db572eb161d7d879b7fbf
Reema Sipra <reema.sipra@gmail.com>
2025-04-12 18:47:28 -0400

    small change - saved files

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ The runner module that executes the game.
 '''
 # Import game modules:
 
-import HPtrivia_game.controller    # Game View and Controller module 
-import src.HPtrivia_game.player  # Player model module
-import src.HPtrivia_game.trivia  # Trivia data module
+# import HPtrivia_game.controller    # Game View and Controller module 
+# import src.HPtrivia_game.player  # Player model module
+# import src.HPtrivia_game.trivia  # Trivia data module
 
 # Start / initialize the game
 # Greet / Introduction
